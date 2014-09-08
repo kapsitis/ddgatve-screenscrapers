c17b40f757c7a75afcf986c85e1a93373c8b8dc3
Kalvis Apsitis <kalvis.apsitis@gmail.com>
2014-09-08 12:32:47 +0300

    modified R

--- src/main/r/locomotive-diagrams.R
+++ src/main/r/locomotive-diagrams.R
@@ -1,5 +1,6 @@
 setwd("/home/kalvis/workspace/ddgatve-screenscrapers/src/main/r")
 
+if (!"plyr" %in% installed.packages()) install.packages("plyr")
 candidateFiles = c("candidates06.csv", "candidates07.csv", "candidates08.csv",
                    "candidates09.csv", "candidates10.csv", "candidates11.csv")
 
@@ -10,10 +11,14 @@ df <- read.csv(paste0("../resources/data-candidates/","candidates-all.csv"))
 #     df <- rbind(df, read.csv(paste0("../resources/data-candidates/",file)))
 #   }
 # }
-
 # write.csv(file="../resources/data-candidates/candidates-all.csv", x=df)
 
 df[,"Saeima"]
 
-# levels(df[,"CandidateName"])
+library(plyr)
+vienotiba10 = subset(df, Saeima == "11" & PartyNum == "1" & DistrNum == "5")
+
+points <- vienotiba10[,"Points"]
+ballots <- vienotiba10[,"BallotsForParty"] 
+plot((points-ballots)/ballots)
 
