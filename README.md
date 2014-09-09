bf745d63696ea661b10c9e8532a48569a36851a1
Kalvis Apsitis <kalvis.apsitis@gmail.com>
2014-09-09 14:10:51 +0300

    Locomotive graphs in R

--- README.md
+++ README.md
@@ -2,3 +2,17 @@ ddgatve-screenscrapers
 ======================
 
 A project to extract election data for Latvian parliament Saeima.
+
+How to run project on Windows
+================================
+(1) Install Git; JDK 1.7; Eclipse
+(2) If you are behind a proxy, run this command: 
+git config --global http.proxy http://your.proxy.com:8081
+(3) Clone the repository - run this command: 
+git clone https://github.com/kapsitis/ddgatve-screenscrapers.git
+(4) Download Typesafe Activator from https://typesafe.com/platform/getstarted
+Select "Minimal package". Unzip. 
+Move the files "activator", "activator.bat" and "activator-launch-1.2.10.jar" 
+to the root directory of ddgatve-screenscrapers
+(5) Fix the dependencies. Change directory to ddgatve-screenscrapers. Run command: 
+activator eclipse
--- src/main/r/locomotive-diagrams.R
+++ src/main/r/locomotive-diagrams.R
@@ -1,10 +1,12 @@
+#setwd("/home/st/ddgatve-screenscrapers/src/main/r")
 setwd("/home/kalvis/workspace/ddgatve-screenscrapers/src/main/r")
 
 if (!"plyr" %in% installed.packages()) install.packages("plyr")
 candidateFiles = c("candidates06.csv", "candidates07.csv", "candidates08.csv",
                    "candidates09.csv", "candidates10.csv", "candidates11.csv")
 
-df <- read.csv(paste0("../resources/data-candidates/","candidates-all.csv"))
+df <- read.csv(paste0("../resources/data-candidates/",
+                      "candidates-all.csv"), fileEncoding="UTF-8")
 
 # for (file in candidateFiles) {
 #   if (file != "candidates06.csv") {
@@ -13,12 +15,40 @@ df <- read.csv(paste0("../resources/data-candidates/","candidates-all.csv"))
 # }
 # write.csv(file="../resources/data-candidates/candidates-all.csv", x=df)
 
-df[,"Saeima"]
+# df[,"Saeima"]
 
 library(plyr)
-vienotiba10 = subset(df, Saeima == "11" & PartyNum == "1" & DistrNum == "5")
+mySaraksts = subset(df, Saeima == "11" & PartyNum == "1")
 
-points <- vienotiba10[,"Points"]
-ballots <- vienotiba10[,"BallotsForParty"] 
-plot((points-ballots)/ballots)
+points <- mySaraksts[,"Points"]
+ballots <- mySaraksts[,"BallotsForParty"] 
+relativeX <- (points-ballots)/ballots
 
+distrNum <- mySaraksts[,"DistrNum"]
+elected <- mySaraksts[,"Result"]
+
+xLower <- -0.35
+xUpper <- 0.35
+
+xx <- c(xLower,xUpper)
+yy <- c(1,5)
+# novadi <- c("R", "V", "L", "K", "Z")
+novadi <- c("Z","K","L","V","R")
+
+plot(xx,yy, type="n", axes = FALSE,
+     xlab="",ylab="")
+title(mySaraksts[1,"PartyAbbr"], sub = "sub title")
+axis(1, seq(xLower,xUpper,by=0.05), seq(xLower,xUpper,by=0.05))
+axis(2, 1:5, novadi)
+
+# Compute for each district separately:
+for (i in 1:5) {
+  dxx <- relativeX[which(distrNum == i)]
+  dyy <- rep(6-i,length(which(distrNum == i)))
+  dElected <- 
+  median <- fivenum(dxx)[3]
+  points(dxx - median,dyy)
+}
+
+grid()
+mySaraksts[,"PartyAbbr"]
